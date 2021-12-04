0876e7c08d8848ef9c1d4cea0f5682c959af40a2
Praveen Kumar <praveenscience@gmail.com>
2021-12-04 15:40:59 +0000

    Implemented a Bootstrap Header Component.

---
+++ src/components/Bootstrap/Header.js
@@ -0,0 +1,20 @@
+const Header = ({ dark, children, className, to, Link }) => {
+  dark = !!dark ? "dark" : "light";
+  return (
+    <nav
+      className={
+        `navbar navbar-${dark} bg-${dark}` + (className ? " " + className : "")
+      }
+    >
+      {to ? (
+        <Link to={to} className="navbar-brand">
+          {children}
+        </Link>
+      ) : (
+        <span className="navbar-brand">{children}</span>
+      )}
+    </nav>
+  );
+};
+
+export default Header;
