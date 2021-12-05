3c92ed1ef5e61eca07f656730f8a076e683c46f5
Praveen Kumar <praveenscience@gmail.com>
2021-12-05 15:12:41 +0000

    Added some Array Elements for other sections.

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,15 @@ class App extends Component {
       KeyWords: "",
       Address: "",
       Phone: "",
-      Email: ""
+      Email: "",
+      Experience: [],
+      Education: [],
+      Skills: {
+        Tools: [],
+        Workflow: []
+      },
+      Interests: [],
+      Awards: []
     }
   };
   toggleHeader = () => {
