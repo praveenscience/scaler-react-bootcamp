edd2ecc441896e5537904f57a74db96f7a9db04b
Praveen Kumar <praveenscience@gmail.com>
2021-12-05 14:46:30 +0000

    Show all the form data.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,13 @@
-const Form = () => {
-  return <div className="Form">Form Here...</div>;
+import FormGroup from "./Bootstrap/FormGroup";
+
+const Form = ({ FormData }) => {
+  return (
+    <div className="Form">
+      {Object.keys(FormData).map(fd => (
+        <FormGroup key={fd} Label={fd} />
+      ))}
+    </div>
+  );
 };
 
 export default Form;
