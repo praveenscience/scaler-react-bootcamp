8e0b794eab2e28524641d72d52e1357ca68b5b21
Praveen Kumar <praveenscience@gmail.com>
2021-12-05 14:34:47 +0000

    Created a FormGroup Component.

---
+++ src/components/Bootstrap/FormGroup.js
@@ -0,0 +1,36 @@
+const FormGroup = ({
+  Id,
+  Label,
+  Type,
+  onChange,
+  Value,
+  defaultValue,
+  Checked,
+  defaultChecked,
+  Desc
+}) => {
+  return (
+    <div className="form-group">
+      <label htmlFor={Id}>{Label}</label>
+      <input
+        type={Type}
+        className="form-control"
+        id={Id}
+        name={Id}
+        onChange={onChange}
+        value={Value}
+        defaultValue={defaultValue}
+        checked={Checked}
+        defaultChecked={defaultChecked}
+        aria-describedby={Id + "Help"}
+      />
+      {Desc && (
+        <small id={Id + "Help"} className="form-text text-muted">
+          {Desc}
+        </small>
+      )}
+    </div>
+  );
+};
+
+export default FormGroup;
