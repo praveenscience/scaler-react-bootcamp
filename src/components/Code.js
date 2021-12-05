32e9199c93e57f618f68b217b2694ec35ecdd256
Praveen Kumar <praveenscience@gmail.com>
2021-12-05 14:20:38 +0000

    Created two functional components for Form and Code.

---
+++ src/components/Code.js
@@ -0,0 +1,5 @@
+const Code = () => {
+  return <div className="Code">Code Here...</div>;
+};
+
+export default Code;
---
+++ src/components/Form.js
@@ -0,0 +1,5 @@
+const Form = () => {
+  return <div className="Form">Form Here...</div>;
+};
+
+export default Form;
